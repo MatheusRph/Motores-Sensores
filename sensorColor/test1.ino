cbd843be8feeca23bc42990a8aaca1d933ae2bec
MatheusRph <matheus.rodolpho16@gmail.com>
2024-10-01 22:43:17 -0300

    Logic corrijed

--- sensorColor/test1.ino
+++ sensorColor/test1.ino
@@ -13,8 +13,10 @@ int calibBB = 0;
 int calibWR = 0;
 int calibWG = 0;
 int calibWB = 0;
+int calibW = 0;
+int calibBL = 0;
 
-int margem = 1;
+int margem = 25;
 
 String ColorTcs;
 
@@ -28,34 +30,29 @@ void calibratorColors() {
     delay(3000);
     uint16_t r, g, b, c;
     colorSensor.getRawData(&r, &g, &b, &c);
-    uint8_t rgb[3];
-    normalizeRGB(r, g, b, rgb);
-    int normalizedC = rgb[1] + rgb[2] + rgb[0];
-    
+       
     Serial.print("Não Calibradas: R=");
     Serial.print(r);
     Serial.print(", G=");
     Serial.print(g);
     Serial.print(", B=");
     Serial.print(b);
-
-    Serial.print("Cores de leitura normalizada R=");
-    Serial.print(rgb[0]);
-    Serial.print(", G=");
-    Serial.print(rgb[1]);
-    Serial.print(", B=");
-    Serial.print(rgb[2]);
-    
+    Serial.print(", C=");
+    Serial.print(c);
+  
     delay(3000);
-    calibR = (normalizedC - (rgb[1] + rgb[2])) / 2;
-    calibG = (normalizedC - (rgb[0] + rgb[2])) / 2;
-    calibB = (normalizedC - (rgb[0] + rgb[1])) / 2;
-    calibBR = std::sqrt((normalizedC - (rgb[1] + rgb[2]))) * 4.03;
-    calibBG = std::sqrt((normalizedC - (rgb[0] + rgb[2]))) * 4.03;
-    calibBB = std::sqrt((normalizedC - (rgb[0] + rgb[1]))) * 4.03;
-    calibWR = (normalizedC - (rgb[1] + rgb[2])) / 1.178;
-    calibWG = (normalizedC - (rgb[0] + rgb[2])) / 1.178;
-    calibWB = (normalizedC - (rgb[0] + rgb[1])) / 1.178;
+    calibR = r / 2;
+    calibG = g / 2;
+    calibB = b / 2;
+    calibBR = std::sqrt(r) * 4.03;
+    calibBG = std::sqrt(g) * 4.03;
+    calibBB = std::sqrt(b) * 4.03;
+    calibBB = std::sqrt(c) * 4.03;
+    calibWR = r / 1.178;
+    calibWG = g / 1.178;
+    calibWB = b / 1.178;
+    calibW = (r + b + g) / 1.178;
+
     Serial.println("Cores calibradas.");
     Serial.print("Calibrações: R=");
     Serial.print(calibR);
@@ -75,36 +72,57 @@ void calibratorColors() {
     Serial.print(calibWG);
     Serial.print(", WB=");
     Serial.println(calibWB);
+    Serial.print(", W=");
+    Serial.println(calibW);
 }
 
-void normalizeRGB(uint16_t r, uint16_t g, uint16_t b, uint8_t *rgb) {
-    delay(1);
-    uint16_t maxValue = max(r, max(g, b));
-    if (maxValue > 0) {
-        rgb[0] = (r * 255) / maxValue; // Normaliza R
-        rgb[1] = (g * 255) / maxValue; // Normaliza G
-        rgb[2] = (b * 255) / maxValue; // Normaliza B
-    } else {
-        rgb[0] = rgb[1] = rgb[2] = 0; // Se todos os valores forem zero
-    }
-    delay(1);
-}
+// void normalizeRGB(uint16_t r, uint16_t g, uint16_t b, uint8_t *rgb) {
+//     delay(1);
+//     uint16_t maxValue = max(r, max(g, b));
+//     if (maxValue > 0) {
+//         rgb[0] = (r * 255) / maxValue; // Normaliza R
+//         rgb[1] = (g * 255) / maxValue; // Normaliza G
+//         rgb[2] = (b * 255) / maxValue; // Normaliza B
+//     } else {
+//         rgb[0] = rgb[1] = rgb[2] = 0; // Se todos os valores forem zero
+//     }
+//     delay(1);
+// }
 
 void identifyColor(uint16_t rp, uint16_t gp, uint16_t bp, uint16_t cp, String &cor) {
     delay(1);
-    if (bp <= (calibBB + margem) &&
-        gp <= (calibBG + margem) &&
-        rp <= (calibBR + margem)) {
+
+    if (bp <= (calibBB) &&
+        gp <= (calibBG) &&
+        rp <= (calibBR)) {
         cor = "Preto"; // Cor detectada: Preto
-    } else if (rp >= (calibR - margem) && gp <= (calibG + margem) && bp <= (calibB + margem)) {
+    } else if (rp >= calibR && gp <= (calibG + margem) && bp <= (calibB + margem)) {
         cor = "Vermelho"; // Cor detectada: Vermelho
-    } else if (gp >= (calibG - margem) && rp <= (calibR + margem) && bp <= (calibB + margem)) {
+    } else if (gp >= calibG && rp <= (calibR + margem) && bp <= (calibB + margem)) {
         cor = "Verde"; // Cor detectada: Verde
-    } else if (bp >= (calibB - margem) && rp <= (calibR + margem) && gp <= (calibG + margem)) {
+    } else if (bp >= calibB && rp <= (calibR + margem) && gp <= (calibG + margem)) {
         cor = "Azul"; // Cor detectada: Azul
+    } else if (rp >= calibWR && gp >= calibWG && bp >= calibWB && cp >= calibW) {
+        cor = "Branco"; // Cor detectada: Branco
     } else {
         cor = "Desconhecida"; // Cor desconhecida
     }
+
+    //     if (bp <= (calibBB + margem) &&
+    //     gp <= (calibBG + margem) &&
+    //     rp <= (calibBR + margem)) {
+    //     cor = "Preto"; // Cor detectada: Preto
+    // } else if (rp >= (calibR + margem) && gp <= (calibG + margem) && bp <= (calibB + margem)) {
+    //     cor = "Vermelho"; // Cor detectada: Vermelho
+    // } else if (gp >= (calibG + margem) && rp <= (calibR + margem) && bp <= (calibB + margem)) {
+    //     cor = "Verde"; // Cor detectada: Verde
+    // } else if (bp >= (calibB + margem) && rp <= (calibR + margem) && gp <= (calibG + margem)) {
+    //     cor = "Azul"; // Cor detectada: Azul
+    // } else if (rp >= calibWR && bp >= calibWG && bp >= calibWB && cp >= calibW) {
+    //     cor = "Branco"; // Cor detectada: Azul
+    // } else {
+    //     cor = "Desconhecida"; // Cor desconhecida
+    // }
 
     //     if (cp < (calibBL + margem) && bp <= (calibB + margem) && gp <= (calibG + margem) && abs(rp - gp) < 35) {
     //     cor = "PRETO"; // Cor detectada: Preto
@@ -122,17 +140,16 @@ void identifyColor(uint16_t rp, uint16_t gp, uint16_t bp, uint16_t cp, String &c
 void readTcs() {
     uint16_t r, g, b, c;
     colorSensor.getRawData(&r, &g, &b, &c);
-    uint8_t rgb[3];
-    normalizeRGB(r, g, b, rgb);
-    int normalizedC = rgb[1] + rgb[2] + rgb[0];
-    identifyColor(rgb[0], rgb[1], rgb[2], normalizedC, ColorTcs);
+    identifyColor(r, g, b, c, ColorTcs);
 
     Serial.print("Cores de leitura R=");
-    Serial.print(rgb[0]);
+    Serial.print(r);
     Serial.print(", G=");
-    Serial.print(rgb[1]);
+    Serial.print(g);
     Serial.print(", B=");
-    Serial.print(rgb[2]);
+    Serial.print(b);
+    Serial.print(", C=");
+    Serial.print(c);
 
     Serial.println("Cor detectada: " + ColorTcs);
 }
